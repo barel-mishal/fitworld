e41b4e90c2016156c7bca3d77317b3cf11c8863a
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-08 07:15:46 +0300

    make

--- src/routes/auth/signout/index.tsx
+++ src/routes/auth/signout/index.tsx
@@ -1,11 +1,24 @@
 import { component$ } from '@builder.io/qwik';
+import { Form } from '@builder.io/qwik-city';
+import { BsGoogle } from '@qwikest/icons/bootstrap';
+import { Button } from '~/components/ui/button/button';
+import { useAuthSignin } from '~/routes/plugin@auth';
 
 
 
 export default component$(() => {
+  const signIn = useAuthSignin();
   return (
     <div>
       New route works.
+      <Form action={signIn} class="self-center">
+        <input type="hidden" name="providerId" value="google" />
+        <input type="hidden" name="options.callbackUrl" value="/dashboard" />
+        <Button class={"group bg-sky-950 outline-1 outline hover:outline-sky-950 text-sky-200 gap-4 hover:bg-sky-100 hover:text-sky-950 transition-all delay-75 duration-200 ease-in-out"}>
+          <BsGoogle/>
+          <p class="text-sky-100 group-hover:text-sky-950 transition-all delay-75 duration-200 ease-in-out">Sign In with Google</p>
+        </Button>
+      </Form>
     </div>
   );
 });
--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -17,8 +17,8 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
       signIn: '/auth/signin',
       signOut: '/auth/signout',
       error: '/auth/error', // Error code passed in query string as ?error=
-      verifyRequest: '/auth/verify-request', // (used for check email message)
-      newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+      // verifyRequest: '/auth/verify-request', // (used for check email message)
+      // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
     }
   }));
 
