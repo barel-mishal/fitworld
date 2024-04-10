f4cdee4a7b4ec9a122e07922b3de62326c35512b
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 11:18:43 +0300

    Add NoteComp component and refactor NotesContainer

---
+++ src/components/layout_blocks/notes_layout_components/NoteComp.tsx
@@ -0,0 +1,37 @@
+import { component$, useContext } from "@builder.io/qwik";
+import { NotesLayoutContext } from "./NotesContext";
+import { NotesLayoutAside } from "./SideBarNotes";
+import { Button } from "~/components/ui/button/button";
+import { fetchDelete, fetchPost, fetchPut } from "~/routes/dashboard/notes";
+import { Textarea } from "~/components/ui/textarea/textarea";
+
+export const NotesContainer = component$(() => {
+    const notesState = useContext(NotesLayoutContext);
+    if (!notesState.store) {
+        return <div>Not found</div>;
+    }
+    return (
+        <div class="flex gap-2  ">
+            <NotesLayoutAside notes={notesState.dataNotes} selectedNoteId={notesState.location.params.id} />
+        <section class={"flex overflow-y-auto flex-grow justify-center"} style={{height: `${notesState.dashboardContext.value.height}px`}}>
+          <div class="">
+            {notesState.store.edit ? <Textarea onInput$={(e, el) => notesState.store.updateContext(el.value)} value={notesState.store.content}></Textarea> : 
+            <div 
+            contentEditable='inherit' class="prose max-w-[600px] px-14 py-12 border" 
+            dangerouslySetInnerHTML={notesState.parsedMarkdown} ></div>}
+  
+            <Button onClick$={() => notesState.store.toggleEdit()}>{notesState.store.textEdit()}</Button>
+            <Button onClick$={async () => {
+              await fetchDelete("1")
+            }}>Delete</Button>
+            <Button onClick$={async () => {
+              await fetchPut("1")
+            }}>Edit</Button>
+            <Button onClick$={async () => {
+              await fetchPost("1")
+            }}>Save</Button>
+          </div>
+        </section>
+      </div>
+    )
+});
--- src/components/layout_blocks/notes_layout_components/notes.tsx
+++ src/components/layout_blocks/notes_layout_components/notes.tsx
@@ -1,9 +1,9 @@
-import { component$, useContext, useContextProvider } from "@builder.io/qwik";
+import { component$, useContextProvider } from "@builder.io/qwik";
 import { Button } from "~/components/ui/button/button";
-import { Textarea } from "~/components/ui/textarea/textarea";
-import { fetchDelete, fetchPost, fetchPut } from "~/routes/dashboard/notes";
+import { fetchPost } from "~/routes/dashboard/notes";
 import { NotesLayoutContext, useNote } from "./NotesContext";
 import { NotesLayoutAside } from "./SideBarNotes";
+import { NotesContainer } from "./NoteComp";
 
 
 export interface NoteProps {
@@ -30,34 +30,5 @@ export const NotesLayout = component$<{note: NoteProps | undefined}>((props) => 
         <NotesContainer />
     )
 });
-export const NotesContainer = component$(() => {
-    const notesState = useContext(NotesLayoutContext);
-    if (!notesState.store) {
-        return <div>Not found</div>;
-    }
-    return (
-        <div class="flex gap-2  ">
-            <NotesLayoutAside notes={notesState.dataNotes} selectedNoteId={notesState.location.params.id} />
-        <section class={"flex overflow-y-auto flex-grow justify-center"} style={{height: `${notesState.dashboardContext.value.height}px`}}>
-          <div class="">
-            {notesState.store.edit ? <Textarea onInput$={(e, el) => notesState.store.updateContext(el.value)} value={notesState.store.content}></Textarea> : 
-            <div 
-            contentEditable='inherit' class="prose max-w-[600px] px-14 py-12 border" 
-            dangerouslySetInnerHTML={notesState.parsedMarkdown} ></div>}
-  
-            <Button onClick$={() => notesState.store.toggleEdit()}>{notesState.store.textEdit()}</Button>
-            <Button onClick$={async () => {
-              await fetchDelete("1")
-            }}>Delete</Button>
-            <Button onClick$={async () => {
-              await fetchPut("1")
-            }}>Edit</Button>
-            <Button onClick$={async () => {
-              await fetchPost("1")
-            }}>Save</Button>
-          </div>
-        </section>
-      </div>
-    )
-});
+
 
