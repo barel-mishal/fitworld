18d06e570df2c2f721508f0e4d506e0c33567137
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 18:10:20 +0300

    adding page

--- src/routes/dashboard/profile/index.tsx
+++ src/routes/dashboard/profile/index.tsx
@@ -1,9 +1,11 @@
 import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
     <div>
       New route works.
+      <Link href="/dashboard/profile/settings">Profile</Link>
     </div>
   );
 });
