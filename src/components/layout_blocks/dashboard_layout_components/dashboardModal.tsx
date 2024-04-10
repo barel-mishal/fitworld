336cc9d1210e35ce77d9d273ac9352e5f08f9704
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 14:32:05 +0300

    something

--- src/components/layout_blocks/dashboard_layout_components/dashboardModal.tsx
+++ src/components/layout_blocks/dashboard_layout_components/dashboardModal.tsx
@@ -11,7 +11,10 @@ export const MyModal = component$<AuthProps>((props) => {
     return (
         <ModalWrapper>
             <ModalTrigger>
-                <Button class={"rounded-full w-12 h-12"}>B</Button>
+                <Button class={"rounded-full w-12 h-12 relative"}>
+                    <span class="absolute h-2 w-2 bg-rose-500 top-0 left-0 rounded-full"></span>
+                    <p>B</p>
+                </Button>
             </ModalTrigger>
             <Modal>
                 <ModalHeader>
