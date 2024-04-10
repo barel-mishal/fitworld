3156fe9902ec385a699987b465b8ed838eb8547f
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 18:36:17 +0300

    Refactor serverData object in service.ts

--- src/routes/api/service.ts
+++ src/routes/api/service.ts
@@ -1,53 +1,59 @@
 import { server$ } from "@builder.io/qwik-city";
 
-export const notes = {
+export const serverData = {
   notes: [
-    { id: "1", text: `Note 1 # Heading one
+    { 
+        id: "1", 
+        text: `Note 1 # Heading one
 
-    ___
-    
-    # heading two
-    
-    ___
-    
-    # Heading one
-    
-    ___
-    
-    # heading two
-    
-    ___
-    
-    # Heading one
-    
-    ___
-    
-    # heading two
-    
-    ___
-    
-    # Heading one
-    
-    ___
-    
-    # heading two
-    
-    ___
-    
-    # Heading one
-    
-    ___
-    
-    # heading two
-    
-    ___`, title: "Title 1" },
+      ___
+      
+      # heading two
+      
+      ___
+      
+      # Heading one
+      
+      ___
+      
+      # heading two
+      
+      ___
+      
+      # Heading one
+      
+      ___
+      
+      # heading two
+      
+      ___
+      
+      # Heading one
+      
+      ___
+      
+      # heading two
+      
+      ___
+      
+      # Heading one
+      
+      ___
+      
+      # heading two
+      
+      ___`, 
+      title: "Title 1",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
     { id: "2", text: "Note 2", title: "Title 2" },
     { id: "3", text: "Note 3", title: "Title 3" },
   ],
 };
 
 export const serverNotes = server$(async function() {
-  return notes
+  return serverData
 });
 export const serverGetNote = server$(async function(id: string) {
     return await serverNotes().then((data) => {
@@ -56,6 +62,6 @@ export const serverGetNote = server$(async function(id: string) {
     });
 });
 export const serverNewNote = server$(async function() {
-    notes.notes = [...notes.notes, { id: (notes.notes.length + 2).toString(), title: "Note " + notes.notes.length, text: "" }]
-    return {note: notes.notes.at(-1)}
+    serverData.notes = [...serverData.notes, { id: (serverData.notes.length + 2).toString(), title: "Note " + serverData.notes.length, text: "" }]
+    return {note: serverData.notes.at(-1)}
 });
