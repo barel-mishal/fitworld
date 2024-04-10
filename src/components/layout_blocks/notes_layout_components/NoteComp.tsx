56e0c8cf5dd0b171eaeec84ac3aa421c7a7f4238
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 15:59:36 +0300

    organize data

--- src/components/layout_blocks/notes_layout_components/NoteComp.tsx
+++ src/components/layout_blocks/notes_layout_components/NoteComp.tsx
@@ -15,20 +15,28 @@ export const NotesContainer = component$(() => {
         <div class="flex gap-2 flex-grow  ">
             <NotesLayoutAside notes={notesState.dataNotes} selectedNoteId={notesState.location.params.id} />
         <section class={"flex overflow-y-auto flex-grow justify-center"} style={{height: `${notesState.dashboardContext.value.height}px`}}>
-          <div class="">
-            {notesState.store.edit ? <Textarea onInput$={(e, el) => notesState.store.updateContext(el.value)} value={notesState.store.content}></Textarea> : 
-            <div 
-            contentEditable='inherit' class="prose max-w-[600px] px-14 py-12 border" 
-            dangerouslySetInnerHTML={notesState.parsedMarkdown} ></div>}
+          <div class="grid grid-cols-6 p-4 gap-4 grid-rows-[auto,1fr]">
+                <Button 
+                class=""
+                onClick$={() => notesState.store.toggleEdit()}>
+                  {notesState.store.textEdit()}
+                </Button>
+                <Button onClick$={async () => {
+                  await fetchDelete("1")
+                }}>Delete</Button>
+                
+                <Button onClick$={async () => {
+                  await fetchPut("1")
+                }}>Edit</Button>
+                
+                <Link href="/dashboard/notes/new">New note</Link>
+            <div class="col-span-6">
+              {notesState.store.edit ? <Textarea class="h-full" onInput$={(e, el) => notesState.store.updateContext(el.value)} value={notesState.store.content}></Textarea> : 
+              <div 
+              contentEditable='inherit' class="prose max-w-[600px] px-14 py-12 border" dangerouslySetInnerHTML={notesState.parsedMarkdown} ></div>}
+            </div>
   
-            <Button onClick$={() => notesState.store.toggleEdit()}>{notesState.store.textEdit()}</Button>
-            <Button onClick$={async () => {
-              await fetchDelete("1")
-            }}>Delete</Button>
-            <Button onClick$={async () => {
-              await fetchPut("1")
-            }}>Edit</Button>
-            <Link href="/dashboard/notes/new">Save</Link>
+          
           </div>
         </section>
       </div>
