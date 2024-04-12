e9ba1a20aab3fc9a517ca2bf2ab2845f9102d3ea
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-12 23:56:35 +0300

    Refactor current-weight component and add height unit popover

--- src/routes/client/Assessment/personalInformation/current-weight/index.tsx
+++ src/routes/client/Assessment/personalInformation/current-weight/index.tsx
@@ -1,9 +1,114 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext, useSignal, useComputed$ } from '@builder.io/qwik';
+import { cn } from '@qwik-ui/utils';
+import { Label } from '~/components/ui/label/label';
+import { contextAssessmentStore } from '../../layout';
+import { PopoverTrigger } from '@qwik-ui/headless';
+import { buttonVariants } from '~/components/ui/button/button';
+import { Popover } from '~/components/ui/popover/popover';
+import { convertUnits } from '~/util/convertUnits';
+import { formatedNumber } from '~/util/formatNumber';
+
+
 
 export default component$(() => {
+  const sc = useContext(contextAssessmentStore);
+
+  const computeHeight = useComputed$(async () => `${formatedNumber(convertUnits(170, "cm", sc.personalInformation.height.type))}`);
+  const inputHeightValue = () => sc.personalInformation.height.value ? `${formatedNumber(sc.personalInformation.height.value)}` : "";
   return (
-    <div>
-      New route works.
+    <div class="grid grid-cols-[1fr,auto] gap-4 w-full">
+
+      <div class="grid max-w-sm items-center gap-1.5 ">
+        <Label for="email-2" class="text-emerald-100">Weight</Label>
+        <input type="Height" id="Height" placeholder={computeHeight.value.toString()} onInput$={async (e,el) => {
+          const height = parseFloat(el.value);
+          sc.personalInformation.height.value = height;
+          }} class={cn(
+            "flex h-12 w-full rounded-base border border-input bg-background px-3 py-1 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          )} value={inputHeightValue()}
+        />
+
+        <p class="text-sm text-emerald-200/70 h-5">The name shown by others in the application </p>
+      </div>
+      <div class="grid max-w-sm items-center gap-1.5 ">
+        <Label for="email-2" class="text-emerald-100">Unit</Label>
+        <MyPopover height={sc.personalInformation.height} />
+      </div>
+
     </div>
+  );
+});
+
+interface HeightGetter {
+  height?: {
+    type: "cm";
+    value: number;
+} | {
+    type: "m";
+    value: number;
+} | {
+    type: "FT";
+    value: number;
+} | undefined
+}
+
+export const MyPopover = component$<HeightGetter>(() => {
+  const sc = useContext(contextAssessmentStore);
+  const triggerRef = useSignal<HTMLButtonElement>();
+  const popoverRef = useSignal<HTMLElement>();
+  const getHeightUnit = () => {
+    switch (sc.personalInformation.height.type) {
+      case "cm":
+      case "m":
+        return "Metric";
+      default:
+        return "Imperial";
+    }
+  };
+
+  return (
+    <>
+      <PopoverTrigger
+        ref={triggerRef}
+        class={buttonVariants({ look: 'outline' })}
+        popovertarget="hero-id"
+      >
+        <span>
+          {sc.personalInformation.height.type.toUpperCase()}
+        </span>
+      </PopoverTrigger>
+      <Popover
+        flip={false}
+        class=""
+        gutter={4}
+        ref={popoverRef}
+        anchorRef={triggerRef}
+        floating={true}
+        placement="bottom"
+        id="hero-id"
+      >
+        <div class="grid gap-4 w-auto">
+          <button onClick$={() => {
+            sc.personalInformation.height = {type: "cm", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "cm")};
+          }}>
+            <span>CM</span>
+          </button>
+
+          <button onClick$={() => {
+            sc.personalInformation.height = {type: "m", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "m")};
+          }}>
+            <span>M</span>
+          </button>
+
+          <button onClick$={() => {
+            sc.personalInformation.height = {type: "FT", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "FT")};
+          }}>
+            <span>FT</span>
+          </button>
+
+        </div>
+      </Popover>
+      <p class="text-sm text-emerald-200/70 h-5">{getHeightUnit()}</p>
+    </>
   );
 });
--- src/routes/client/Assessment/personalInformation/height/index.tsx
+++ src/routes/client/Assessment/personalInformation/height/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useContext, useSignal, useComputed$ } from '@builder.io/qwik';
+import { component$, useContext, useSignal, useComputed$, useOn, $ } from '@builder.io/qwik';
 import { cn } from '@qwik-ui/utils';
 import { Label } from '~/components/ui/label/label';
 import { contextAssessmentStore } from '../../layout';
@@ -12,20 +12,46 @@ import { formatedNumber } from '~/util/formatNumber';
 
 export default component$(() => {
   const sc = useContext(contextAssessmentStore);
+  const refHeight = useSignal<HTMLButtonElement>();
+  const isAvtive = useSignal<boolean>(false);
 
-  const computeHeight = useComputed$(async () => `${formatedNumber(convertUnits(170, "cm", sc.personalInformation.height.type))}${sc.personalInformation.height.type}`);
-  const inputHeightValue = () => sc.personalInformation.height.value ? `${formatedNumber(sc.personalInformation.height.value)}${sc.personalInformation.height.type}` : "";
+  const computeHeight = useComputed$(async () => `${formatedNumber(convertUnits(170, "cm", sc.personalInformation.height.type))}`);
+  const inputHeightValue = () => {
+    const isActiveAndEmpty = isAvtive.value && !sc.personalInformation.height.value;
+    if (isActiveAndEmpty) {
+      return "";
+    }
+    const isActiveAndNotEmpty = isAvtive.value && sc.personalInformation.height.value;
+    if (isActiveAndNotEmpty) {
+      console.log("active and not empty");
+      return sc.personalInformation.height.value;
+    }
+    const isNotActiveAndNotEmpty = !isAvtive.value && sc.personalInformation.height.value;
+    if (isNotActiveAndNotEmpty) {
+      return `${formatedNumber(sc.personalInformation.height.value)}`;
+    }
+    return "";
+  }
+
   return (
     <div class="grid grid-cols-[1fr,auto] gap-4 w-full">
 
       <div class="grid max-w-sm items-center gap-1.5 ">
         <Label for="email-2" class="text-emerald-100">Height</Label>
-        <input type="Height" id="Height" placeholder={computeHeight.value.toString()} onInput$={async (e,el) => {
+        <input 
+        ref={refHeight}
+        type="Height" id="Height" placeholder={computeHeight.value.toString()} onInput$={async (e,el) => {
           const height = parseFloat(el.value);
           sc.personalInformation.height.value = height;
           }} class={cn(
             "flex h-12 w-full rounded-base border border-input bg-background px-3 py-1 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           )} value={inputHeightValue()}
+          onFocus$={() => {
+            isAvtive.value = true;
+          }}
+          onBlur$={() => {
+            isAvtive.value = false;
+          }}
         />
 
         <p class="text-sm text-emerald-200/70 h-5">The name shown by others in the application </p>
@@ -89,19 +115,19 @@ export const MyPopover = component$<HeightGetter>(() => {
       >
         <div class="grid gap-4 w-auto">
           <button onClick$={() => {
-            sc.personalInformation.height = {type: "cm", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "cm")};
+            sc.personalInformation.height = {type: "cm", value: parseFloat(formatedNumber(convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "cm")))};
           }}>
             <span>CM</span>
           </button>
 
           <button onClick$={() => {
-            sc.personalInformation.height = {type: "m", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "m")};
+            sc.personalInformation.height = {type: "m", value: parseFloat(formatedNumber(convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "m")))};
           }}>
             <span>M</span>
           </button>
 
           <button onClick$={() => {
-            sc.personalInformation.height = {type: "FT", value: convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "FT")};
+            sc.personalInformation.height = {type: "FT", value: parseFloat(formatedNumber(convertUnits(sc.personalInformation.height.value, sc.personalInformation.height.type, "FT")))};
           }}>
             <span>FT</span>
           </button>
