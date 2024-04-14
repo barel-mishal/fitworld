b6cda07930447690064ce2aa1564d95d0b3d3dfa
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-14 21:33:22 +0300

    Update grid row height in play/index.tsx

--- src/routes/client/(main)/play/index.tsx
+++ src/routes/client/(main)/play/index.tsx
@@ -12,7 +12,7 @@ export default component$(() => {
   
     <HeaderMainBottomNav 
     classMain='tw '
-    class="grid-rows-[40px,1fr,40px]">
+    class="grid-rows-[40px,1fr,30px]">
       <div q:slot='header'><TopNavBar /></div>
       <div q:slot='main'><AppLink route="/client/Assessment/">Section Getting started</AppLink></div>
       <div q:slot='footer' class=""><BottomNavBar flag={{class: "--tw bg-sky-300/20 p-1 rounded-md outline-2 outline outline-indigo-200"}} /></div>
