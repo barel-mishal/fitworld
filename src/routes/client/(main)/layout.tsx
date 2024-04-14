eea2326aec73e3d91d5554e64f81a38651a0aba3
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-14 18:18:11 +0300

    make

--- src/routes/client/(main)/layout.tsx
+++ src/routes/client/(main)/layout.tsx
@@ -1,6 +1,4 @@
 import { Slot, component$ } from '@builder.io/qwik';
-import HeaderMainBottomNav from '~/components/gamelayouts/smallScreens/headerMainBottomNav';
-import { BottomNavBar, TopNavBar } from '~/components/layout_blocks/NavBar/Navs';
 
 
 export default component$(() => {
@@ -9,13 +7,7 @@ export default component$(() => {
   // אנרגיה, חלבון, מים, פיטנס
   // ניווט בין לידרבוארד למסך הראשי
   return (  
-    <HeaderMainBottomNav 
-    classMain='tw '
-    class="grid-rows-[40px,1fr,40px]">
-      <div q:slot='header'><TopNavBar /></div>
-      <div q:slot='main'><Slot /></div>
-      <div q:slot='footer' class=""><BottomNavBar /></div>
-    </HeaderMainBottomNav>
+    <Slot />
   );
 });
 
--- src/routes/client/(main)/user/index.tsx
+++ src/routes/client/(main)/user/index.tsx
@@ -1,4 +1,6 @@
 import { component$ } from '@builder.io/qwik';
+import { cn } from '@qwik-ui/utils';
+import { BottomNavBar } from '~/components/layout_blocks/NavBar/Navs';
 import { MyModalSignOut } from '~/components/layout_blocks/dashboard_layout_components/dashboardModal';
 import { useAuthSession, useAuthSignout } from '~/routes/plugin@auth';
 
@@ -7,10 +9,19 @@ export default component$(() => {
   const signout = useAuthSignout();
   console.log('auth', auth.value);
   return (
-    <div>
-      New route works.
-      <p>{auth.value?.user?.email}</p>
-      <MyModalSignOut name={auth.value?.user?.name ?? "B"} signout={signout} />
+  <div class={cn("grid grid-rows-[1fr,30px] h-screen text-emerald-50 p-3 bg-emerald-950")}>
+    <div class={cn("bg-emerald-950 overflow-y-auto")}>
+        <div >
+          <div>
+            New route works.
+            <p>{auth.value?.user?.email}</p>
+            <MyModalSignOut name={auth.value?.user?.name ?? "B"} signout={signout} />
+          </div>
+        </div>
     </div>
+    <div class="bg-emerald-950 content-center">
+      <div q:slot='footer' class=""><BottomNavBar /></div>
+    </div>
+  </div>
   );
 });
