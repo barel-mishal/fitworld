05cf3b8c90dd2ff88cdcf3b62ccd2122c4022ed0
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-14 19:54:01 +0300

    make

--- src/routes/client/Assessment/layout.tsx
+++ src/routes/client/Assessment/layout.tsx
@@ -1,6 +1,7 @@
 import { type Session } from '@auth/core/types';
-import { $, Slot, component$, createContextId, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
+import { $, Slot, component$, createContextId, useComputed$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import { useLocation, useNavigate, type RequestHandler } from '@builder.io/qwik-city';
+import { cn } from '@qwik-ui/utils';
 import { BsArrowLeft } from '@qwikest/icons/bootstrap';
 import HeaderMainBottomNav from '~/components/gamelayouts/smallScreens/headerMainBottomNav';
 import { Button } from '~/components/ui/button/button';
@@ -86,6 +87,11 @@ export default component$(() => {
   
   const location = useLocation();
   const navigate = useNavigate();
+
+  const isFirst = useComputed$(() => {
+    const current = location.url.pathname;
+    return routes[0] === current;
+  });
   
   const next = $(() => {
     const current = location.url.pathname;
@@ -113,7 +119,7 @@ export default component$(() => {
   return (
   
     <HeaderMainBottomNav >
-      <div q:slot='header' class=""><Button onClick$={prev} look={"ghost"} class="text-emerald-200 p-0 active:bg-transparent hover:bg-transparent "><BsArrowLeft class="" style={{height: 30, width: 30}} /></Button></div>
+      <div q:slot='header' class=""><Button onClick$={prev} look={"ghost"} class={cn("text-emerald-200 p-0 active:bg-transparent hover:bg-transparent ", isFirst.value && "hidden")}><BsArrowLeft class="" style={{height: 30, width: 30}} /></Button></div>
       <div q:slot='main' class=" h-full flex items-center"><Slot /></div>
       <div q:slot='footer'><Button disabled={assessmentStore.settings.buttonDisabled} class="w-full" role={"button"} look={assessmentStore.settings.buttonStyle} size={"md"} onClick$={next}>CONTINUE</Button></div>
     </HeaderMainBottomNav>
