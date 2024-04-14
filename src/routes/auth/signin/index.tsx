26c1acfe03a63f4b9dd0e12dfd98a97569100d2c
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-14 21:20:11 +0300

    client

--- src/routes/auth/signin/index.tsx
+++ src/routes/auth/signin/index.tsx
@@ -3,9 +3,11 @@ import { Form } from '@builder.io/qwik-city';
 import { Button } from '~/components/ui/button/button';
 import { useAuthSignin } from '~/routes/plugin@auth';
 import { BsGoogle, BsCompass } from '@qwikest/icons/bootstrap';
+import { type RoutesLiteral } from '~/util/types';
 
 export default component$(() => {
   const signIn = useAuthSignin();
+  const route: RoutesLiteral = "/client/play/" as "/client/(main)/play/"
   return (
     <main class={"flex flex-col items-center justify-center h-screen bg-sky-100"}>
       <div class={"grid grid-rows-[55px,20px,30px,100px] gap-4 bg-white p-10 rounded-3xl max-w-[500px] lg:min-w-[600px]"}>
@@ -17,7 +19,7 @@ export default component$(() => {
         <p class="text-sky-800">Sign in with your google account</p>
         <Form action={signIn} class="self-center">
           <input type="hidden" name="providerId" value="google" />
-          <input type="hidden" name="options.callbackUrl" value="/dashboard" />
+          <input type="hidden" name="options.callbackUrl" value={route} />
           <Button class={"group bg-sky-950 outline-1 outline hover:outline-sky-950 text-sky-200 gap-4 hover:bg-sky-100 hover:text-sky-950 transition-all delay-75 duration-200 ease-in-out"}>
             <BsGoogle/>
             <p class="text-sky-100 group-hover:text-sky-950 transition-all delay-75 duration-200 ease-in-out">Sign In with Google</p>
