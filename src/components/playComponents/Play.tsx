c6920fe1b338fe2a7eb8c887456a252f526d401c
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-01 08:18:17 +0300

    animetion

--- src/components/playComponents/Play.tsx
+++ src/components/playComponents/Play.tsx
@@ -1,4 +1,4 @@
-import { $, component$, useComputed$, useOn, useSignal } from "@builder.io/qwik";
+import { $, component$, useOn, useSignal } from "@builder.io/qwik";
 import { PhFooPeinapple } from "../icons/icons";
 
 const Play = component$(() => {
@@ -8,17 +8,22 @@ const Play = component$(() => {
         cordients.value = {x: e.clientX, y: e.clientY};
     }))
 
-    const yComputed = useComputed$(() => `${cordients.value.y}px`);
+    // const yComputed = useComputed$(() => `${cordients.value.y}px`);
 
     return (
         <div class="flex flex-col items-center">
+            <div class="h-96 w-96 grid place-content-center card-wrapper">
+                <div class="card-content">
+                    <div>sdlfkj</div>
+                </div>
+            </div>
             <div class="grid grid-cols-3 w-1/2 bg-orange-900 h-80 border-b border-orange-600">
                 <p class="text-emerald-100 text-2xl font-roundsans font-extrabold place-self-center text-center h-full w-full border-r border-orange-800">
                     <span class=""></span>
                 </p>
                 <p class="text-emerald-100 text-2xl font-roundsans font-extrabold place-self-center text-center h-full w-full relative ">
                     <span class="absolute left-1/2 transform -translate-x-1/2"
-                    style={{top: yComputed.value}}><PhFooPeinapple class="h-16 w-16 pt-2" /></span>
+                    style={{}}><PhFooPeinapple class="h-16 w-16 pt-2" /></span>
                 </p>
                 <p class="text-emerald-100 text-2xl font-roundsans font-extrabold place-self-center text-center h-full w-full border-l border-orange-800">
                     <span class=""></span>
