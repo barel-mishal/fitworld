9fb411651e404a69b5d82c272fecc81581bf4b44
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-09 11:48:38 +0300

    delete finish

--- src/routes/client/(main)/track/finish/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, } from '@builder.io/qwik';
-import FinishTrack from '~/components/TrackFood/TrackFinish';
-
-export default component$(() => {
-  return (
-   <FinishTrack />
-  );
-});
-
