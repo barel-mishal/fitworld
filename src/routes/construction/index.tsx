35d672a366011120d8404a108fa9cd7d82b0052e
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-22 22:29:17 +0300

    Refactor routes.gen.d.ts to remove unused /api/service_food_group/ route

--- src/routes/construction/index.tsx
+++ src/routes/construction/index.tsx
@@ -1,8 +1,8 @@
 import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+// import { useLocation } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  const loc = useLocation();
+  // const loc = useLocation();
   return (
     <div class="grid">
       
