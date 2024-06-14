a2e29d07a99f3736d8768f4170ec67859431fa3c
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-06-14 10:59:17 +0300

    refactor: Update user weights component to display weights dynamically

--- frontend/src/routes/client/(main)/user/weights/index.tsx
+++ frontend/src/routes/client/(main)/user/weights/index.tsx
@@ -1,9 +1,43 @@
 import { component$ } from '@builder.io/qwik';
+import { routeLoader$, server$ } from '@builder.io/qwik-city';
+
+
+export const useLoaderUserWeights = routeLoader$(async function () {
+  return {
+    weights: [
+      {
+        date: "2021-10-01",
+        weight: 200,
+
+      },
+      {
+        date: "2021-10-02",
+        weight: 199,
+      }
+    ]
+  }
+});
 
 export default component$(() => {
+  const weights = useLoaderUserWeights().value;
   return (
     <div>
-      New route works. weights
+      {
+        weights.weights.map((weight) => {
+          return (
+            <div key={weight.date}>
+              <p>{weight.date}</p>
+              <p>{weight.weight}</p>
+            </div>
+          );
+        })
+        }
     </div>
   );
 });
+
+export const serverInsertWeight = server$(async function () {
+  return {
+    success: true,
+  };
+})
