4b7966c6f805e485fa01ec43d5b4d74786ef6d33
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-06-18 11:20:09 +0300

    make

--- frontend/src/routes/client/(main)/user/weights/finish/index.tsx
+++ frontend/src/routes/client/(main)/user/weights/finish/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div>
+    <div class="min-h-screen bg-gray-950 p-4 grid gap-3 content-start font-roundsans text-gray-50 grid-rows-[auto,auto,1fr,auto] h-screen">
       New route works.
     </div>
   );
