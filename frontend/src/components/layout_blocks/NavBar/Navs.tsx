9ed22dbeed808740b8786847339afe292304abcd
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-06-19 16:21:34 +0300

    refactor: Update classNames for CloseModal and Play page components

--- frontend/src/components/layout_blocks/NavBar/Navs.tsx
+++ frontend/src/components/layout_blocks/NavBar/Navs.tsx
@@ -13,6 +13,7 @@ import {
 } from "~/components/icons/icons";
 import { AppLink } from "~/routes.config";
 import styles from '~/components/layout_blocks/NavBar/styles.css?inline';
+import { cn } from "@qwik-ui/utils";
 
 interface TopNavBarProps {
   streak: number;
@@ -33,68 +34,76 @@ export const TopNavBar = component$<TopNavBarProps>((props) => {
   return (
     <ul class="grid grid-cols-4">
       <li class="">
-        <Modal.Root closeOnBackdropClick={true}>
-          <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center">
-            <PhLightning class="h-8 fill-yellow-500" />
-            <p class="text-xs">{props.streak}</p>
+      <Modal.Root closeOnBackdropClick={true}>
+          <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center slide-down">
+              <PhLightning class="h-8 fill-yellow-500" />
+              <p class="text-xs">{props.streak}</p>
           </Modal.Trigger>
-          <Modal.Panel class={`
-             modal-panel bottom-sheet w-full h-full p-2 bg-gray-800 
-          `}>
-            <div class="grid ">
-              <Modal.Title>Hello 1</Modal.Title>
-            </div>
-            <Modal.Close class=" "></Modal.Close>
-          </Modal.Panel>
-        </Modal.Root>
+          <Modal.Panel
+            class={cn(
+              `fixed inset-x-0 top-0 h-1/2 w-full bg-gray-950 shadow-lg p-4 m-0 backdrop:bg-slate-400/70 backdrop-blur-sm slide-down backdrop:opacity-90`,
+            )}
+          >
+            <div class="grid">
+            <Modal.Title>Hello 1</Modal.Title>
+            <Modal.Close class="">Close</Modal.Close>
+          </div>
+        </Modal.Panel>
+      </Modal.Root>
       </li>
       <li class="">
       <Modal.Root closeOnBackdropClick={true}>
-          <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center">
+        <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center slide-down">
             <PhDrop class="h-8 fill-blue-500" />
             <p class="text-xs">{props.water}</p>
-          </Modal.Trigger>
-          <Modal.Panel class={`
-             modal-panel bottom-sheet w-full h-full p-2 bg-gray-800 
-          `}>
-            <div class="grid ">
-              <Modal.Title>Hello 1</Modal.Title>
-            </div>
-            <Modal.Close class=" "></Modal.Close>
-          </Modal.Panel>
-        </Modal.Root>
+        </Modal.Trigger>
+        <Modal.Panel
+          class={cn(
+            `fixed inset-x-0 top-0 h-1/2 w-full bg-gray-950 shadow-lg p-4 m-0 backdrop:bg-slate-400/70 backdrop-blur-sm slide-down backdrop:opacity-90`,
+          )}
+        >
+          <div class="grid">
+            <Modal.Title>Hello 1</Modal.Title>
+            <Modal.Close class="">Close</Modal.Close>
+          </div>
+        </Modal.Panel>
+      </Modal.Root>
       </li>
       <li class="">
       <Modal.Root closeOnBackdropClick={true}>
-          <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center">
+        <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center slide-down">
             <PhHeart class="h-8 fill-rose-500" />
             <p class="text-xs">{props.heart}</p>
-          </Modal.Trigger>
-          <Modal.Panel class={`
-             modal-panel bottom-sheet w-full h-full p-2 bg-gray-800 
-          `}>
-            <div class="grid ">
-              <Modal.Title>Hello 1</Modal.Title>
-            </div>
-            <Modal.Close class=" "></Modal.Close>
-          </Modal.Panel>
-        </Modal.Root>
+        </Modal.Trigger>
+        <Modal.Panel
+          class={cn(
+            `fixed inset-x-0 top-0 h-1/2 w-full bg-gray-950 shadow-lg p-4 m-0 backdrop:bg-slate-400/70 backdrop-blur-sm slide-down backdrop:opacity-90`,
+          )}
+        >
+          <div class="grid">
+            <Modal.Title>Hello 1</Modal.Title>
+            <Modal.Close class="">Close</Modal.Close>
+          </div>
+        </Modal.Panel>
+      </Modal.Root>
       </li>
       <li class="">
-        <Modal.Root closeOnBackdropClick={true}>
-          <Modal.Trigger class="modal-trigger grid grid-cols-2 items-center">
-            <PhDNA class="h-8 fill-green-500" />
-            <p class="text-xs">{props.dna}</p>
-          </Modal.Trigger>
-          <Modal.Panel class={`
-             modal-panel bottom-sheet w-full h-full p-2 bg-gray-800 
-          `}>
-            <div class="grid ">
-              <Modal.Title>Hello 1</Modal.Title>
-            </div>
-            <Modal.Close class=" "></Modal.Close>
-          </Modal.Panel>
-        </Modal.Root>
+      <Modal.Root closeOnBackdropClick={true}>
+        <Modal.Trigger class="grid grid-cols-2 items-center slide-down">
+          <PhDNA class="h-8 fill-green-500" />
+          <p class="text-xs">{props.dna}</p>
+        </Modal.Trigger>
+        <Modal.Panel
+          class={cn(
+            `fixed inset-x-0 top-0 h-1/2 w-full bg-gray-950 shadow-lg p-4 m-0 backdrop:bg-slate-400/70 backdrop-blur-sm slide-down backdrop:opacity-90`,
+          )}
+        >
+          <div class="grid">
+            <Modal.Title>Hello 1</Modal.Title>
+            <Modal.Close class="">Close</Modal.Close>
+          </div>
+        </Modal.Panel>
+      </Modal.Root>
       </li>
     </ul>
   );
