e7f61e3c6660cb12e4af01422dc0f6ea315a263f
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-06-25 15:03:31 +0300

    refactor: Update sign-in page layout and remove unused code

--- frontend/src/routes/auth/signin/index.tsx
+++ frontend/src/routes/auth/signin/index.tsx
@@ -4,54 +4,41 @@ import { Button } from "~/components/ui/button/button";
 import { useAuthSignin } from "~/routes/plugin@auth";
 import { BsGoogle } from "@qwikest/icons/bootstrap";
 import { type RoutesLiteral } from "~/util/types";
+import SmailFoo from "~/media/FooSmailPeinApple.png?jsx";
 
 export default component$(() => {
   const signIn = useAuthSignin();
   const route: RoutesLiteral = "/client/play/" as "/client/(main)/play/";
   return (
 
-    <main class="w-full h-full ">
-      <div class="grid ">         
-        <Form action={signIn} class="self-center">
+    <main class="w-full grid place-content-center bg-cover bg-center relative h-screen">
+      <div class="absolute inset-0 bg-gradient-to-b from-gray-800 to-transparent opacity-75"></div>
+      <div class="relative  z-10 bg-white dark:bg-gray-900 p-8 rounded-lg shadow-2xl w-full max-w-md md:max-w-lg lg:max-w-xl transform transition-all duration-300 ease-in-out hover:scale-105">
+        <h1 class="text-4xl font-extrabold text-center mb-5 dark:text-yellow-200 text-sky-950">
+          Welcome to FoodIt.Health
+        </h1>
+        <div class="flex justify-center mb-5">
+          <SmailFoo class="w-32 h-32 md:w-40 md:h-40" />
+        </div>
+        <p class="text-center mb-5 text-gray-700 dark:text-gray-300">
+          Hi there! I am Foo. Let's connect and start our journey to a healthier you.
+        </p>
+        <Form action={signIn} class="flex flex-col items-center">
           <input type="hidden" name="providerId" value="google" />
           <input type="hidden" name="options.callbackUrl" value={route} />
           <Button
-            class={
-              "flex items-center gap-2 "
-            }
+            type="submit"
+            class="flex items-center gap-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg transition-colors duration-200 transform hover:scale-105"
           >
-            <BsGoogle />
-            <p class="text-sky-100 transition-all delay-75 duration-200 ease-in-out group-hover:text-sky-950">
-              Sign In with Google
-            </p>
+            <BsGoogle class="text-xl" />
+            <span class="text-sky-100 transition-all delay-75 duration-200 ease-in-out group-hover:text-sky-950">
+              Connect with Google
+            </span>
           </Button>
         </Form>
       </div>
-
     </main>
-    // <main
-    //   class={"flex h-screen flex-col items-center justify-center bg-sky-100"}
-    // >
-    //   <div
-    //     class={
-    //       "grid max-w-[500px] grid-rows-[55px,20px,30px,100px] gap-4 rounded-3xl bg-white p-10 lg:min-w-[600px]"
-    //     }
-    //   >
-    //     <div class="flex items-center gap-2">
-    //       <BsCompass class="h-10 w-10 fill-sky-800" width={30} height={30} />
-    //       <h1 class="text-3xl font-bold text-sky-950">FitWorld</h1>
-    //     </div>
-    //     <h1 class="text-xl font-semibold text-sky-700">Sign In</h1>
-    //     <p class="text-sky-800">Sign in with your google account</p>
-    //     <p class="flex flex-col gap-2 text-sky-800">
-    //       <span class="[text-wrap:balance]">
-    //         Not your computer? Use a private browsing window to sign in.
-    //       </span>
-    //       <a href="#" class="text-sky-600 underline">
-    //         Learn more
-    //       </a>
-    //     </p>
-    //   </div>
-    // </main>
   );
 });
+
+
