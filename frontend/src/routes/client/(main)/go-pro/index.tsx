924562debfc9fdf2a5f245e9d16119bee7c4bafe
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-06-30 01:56:01 +0300

    refactor: Add lazy loading for images in Go Pro page

--- frontend/src/routes/client/(main)/go-pro/index.tsx
+++ frontend/src/routes/client/(main)/go-pro/index.tsx
@@ -1,11 +1,25 @@
-import { component$, useId, useVisibleTask$ } from '@builder.io/qwik';
+import { component$, useId, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 import { loadScript } from '@paypal/paypal-js';
+import FoodCool from "~/media/CoolFooPeinApple.png?jsx";
 
 export default component$(() => {
   return (
-    <div>
-      New route works.
-      <Paypal />
+    <div class="bg-gray-100 dark:bg-gray-950 min-h-screen flex flex-col items-center justify-center p-4">
+      <div class="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-8 max-w-2xl mx-auto  text-center">
+        <header class="mb-6 flex flex-col gap-3">
+          <FoodCool class="h-24 w-24 mx-auto" />
+          <h1 class="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-2">Go Pro</h1>
+          <p class="text-xl text-gray-600 dark:text-gray-200">Unlock premium features and elevate your experience.</p>
+        </header>
+        <section class="mb-6">
+          <p class="text-gray-700 dark:text-gray-300 mb-4">
+            Upgrade to Pro and enjoy exclusive benefits such as enhanced functionality, priority support, and more.
+          </p>
+        </section>
+        <footer class="mt-6">
+          <Paypal />
+        </footer>
+      </div>
     </div>
   );
 });
@@ -14,6 +28,7 @@ export default component$(() => {
 
 export const Paypal = component$(() => {
   const idPaypal = useId();
+  const active = useSignal(false);
   // Load PayPal script when the component is visible
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(() => {
@@ -67,6 +82,8 @@ export const Paypal = component$(() => {
           .render(`#${idPaypal}`)
           .then(() => {
             console.log('PayPal Buttons rendered');
+            
+            active.value = true;
           })
           .catch((error) => {
             console.error('Failed to render the PayPal Buttons', error);
@@ -80,8 +97,11 @@ export const Paypal = component$(() => {
   });
 
   return (
-    <>
-      <div id={`${idPaypal}`}></div>
-    </>
+  
+    <div class="p-4 grid">
+      <div class="col-start-1 row-start-1 w-full h-32 duration-600 transition-opacity ease-in-out bg-white dark:bg-gray-800 data-[active='true']:opacity-0 grid z-[10000]" data-active={`${active.value}`}></div>
+      <div class="col-start-1 row-start-1" id={`${idPaypal}`}></div>
+    </div>
+  
   );
 });
